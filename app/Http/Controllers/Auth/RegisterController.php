fca8edcba96164dd89c1575acf405fc57c5c92cf
kenta.maeda <km86.sigurros@icloud.com>
2021-02-24 22:31:42 +0000

    registerform

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8'],
+             'gender' => ['required'],
+             'like' => ['required'],
         ]);
     }
 
@@ -68,6 +70,9 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+               'gender'   => $data['gender'],
+               'old'      => $data['old'],
+               'like'   => $data['like'],
         ]);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,10 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('old')->default(0);
+            $table->string('old')->default("未設定");
             $table->string('free')->default("はじめまして。これからよろしくお願いします。");
-            $table->string('gender')->default(0);
-            $table->string('like')->default(0);
+            $table->string('gender')->default("未設定");
+            $table->string('like')->default("未設定");
             $table->string('type')->default(1);
             $table->rememberToken();
             $table->timestamps();
